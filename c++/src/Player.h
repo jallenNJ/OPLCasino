7f7f6a033a764329ba130a24a3b0ff4448aa295e
jallenNJ <33922179+jallenNJ@users.noreply.github.com>
2018-09-09 11:51:06 -0400

    Moved Enums to Parent Class

--- c++/src/Player.h
+++ c++/src/Player.h
@@ -1,13 +1,15 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <string>
-
+#include <vector>
+#include "Card.h"
 using namespace std;
 
  class Player {
 public:
+	enum Actions { Capture, Build, Trail };
 	Player();
-	virtual int doTurn(int) = 0 ;
+	virtual int doTurn(vector<Card>, vector<Card>) = 0 ;
 	string getName() {
 		return name;
 	}

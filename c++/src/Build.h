f4c2517d0f1154dbded381f927316ea25914011e
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2018-09-18 12:34:54 -0400

    Added comments to Build

--- c++/src/Build.h
+++ c++/src/Build.h
@@ -9,6 +9,22 @@ public:
 	Build() {
 		
 	}
+
+
+/* *********************************************************************
+Function Name: Build()
+Purpose: Constructor that takes in information to create a build
+Parameters:
+			Card First, A card passed by value which will be copied as the first card in the build
+			player, a string, which is the name of the player who owns the build
+Return Value: Constructor[Object itself]
+Local Variables:
+			n/a
+Algorithm:
+			1) Assigned all variables to the appropriate intialized value
+			2) Copy first into the main vector
+Assistance Received: none
+********************************************************************* */
 	Build(Card first, string player) {
 		ownerName = player;
 		cardsInBuild.push_back(first);
@@ -16,6 +32,21 @@ public:
 		symbol = getSymbol();
 
 	}
+
+/* *********************************************************************
+Function Name: toString
+Purpose: To format the object to be printed out when being displayed on the table
+Parameters:
+			void
+Return Value: The formatted object. Will be
+	[ Card0 Card1 Card2 ] where Card0 is the card in the 0th index of the vector calling its own toString() function
+Local Variables:
+			string convertted, the formatted string as its being built
+Algorithm:
+			1) Places the outside brackers []
+			2) Loop through all cards in the build and call their toString function()
+Assistance Received: none
+********************************************************************* */
 
 	virtual const string toString() const override {
 		string convertted = "[ ";
@@ -26,9 +57,24 @@ public:
 		return convertted;
 	}
 
+	/* *********************************************************************
+Function Name: getSymbol
+Purpose: To get the symbol that represents the value of the build
+Parameters:
+			void
+Return Value: a const char that is 2-9 or A,X,J,Q,K
+Local Variables:
+			nValue, an int, which reprents the sum of all cards in the build
+Algorithm:
+			1) Get the numeric value of the build
+			2) Return the symbol based on the value
+Assistance Received: none
+********************************************************************* */
+
 	const virtual char getSymbol() const override {
 		int nValue = getNumericValue();
 		switch (nValue) {
+			//Aces are both 1 and 14, therefore the same symbol
 			case 1:
 			case 14:
 				return 'A';
@@ -45,6 +91,20 @@ public:
 		}
 	}
 
+	/* *********************************************************************
+Function Name: getNumericValue
+Purpose: To get the sum of all Cards in the build
+Parameters:
+			void
+Return Value: A const int, of the value of the build. [2-14] are valid values
+Local Variables:
+			sum, an int, the summation of all the cards
+Algorithm:
+			1) Sum the value of all the cards
+			2) Return the value
+Assistance Received: none
+********************************************************************* */
+
 	const virtual int getNumericValue() const override {
 		int sum = 0;
 		for (unsigned int i = 0; i < cardsInBuild.size(); i++) {
@@ -53,8 +113,20 @@ public:
 		return sum;
 	}
 
+	/* *********************************************************************
+Function Name: addCardToBuild
+Purpose: To add another card into the build
+Parameters:
+			newCards, a Card passed by value. If this card doesn't exceed 14, it will be added
+Return Value: bool. True if added, false if rejected
+Local Variables:
+			n/a
+Algorithm:
+			1) Check to make sure the Card is valid
+			2) Add to data structure
+Assistance Received: none
+********************************************************************* */
 	inline bool addCardToBuild(Card newCard) {
-		//TODO: Add check to make sure doesn't exceed 14 etc
 		if (newCard.getNumericValue() + getNumericValue() > 14) {
 			return false;
 		}
--- c++/src/Casino.cpp
+++ c++/src/Casino.cpp
@@ -1,6 +1,27 @@
 #include "Casino.h"
 
 
+/************************************************************
+* Name:  Joseph Allen   jallen6@ramapo.edu                 *
+* Project : Casino Game c++ Submission                     *
+* Class : OPL Section-01                                   *
+* Date : 2018-10-02                                        *
+************************************************************/
+
+/* *********************************************************************
+Function Name: main
+Purpose: The main accesspoint to the program
+Parameters:
+			argc, int size of argv
+			argv, Array of charcter arrays for eaach command line argument
+Return Value: The exit code of the program, an int
+Local Variables:
+			Tournament* tour, which contains the game to be run
+Algorithm:
+			1) Create the tournament object
+			2) Tell the object to start running
+Assistance Received: none
+********************************************************************* */
 int main(int argc, char* argv[]) {
 
 	Tournament* tour = new Tournament;

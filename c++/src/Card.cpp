2becd9ec2d5d47dd63052be8dc0af36357470689
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2018-09-18 13:02:07 -0400

    Commented the Card Class

--- c++/src/Card.cpp
+++ c++/src/Card.cpp
@@ -5,30 +5,64 @@ const bool Card::checkCapture(Card other) {
 	return getSymbol() == other.getSymbol();
 }
 
+
+/* *********************************************************************
+Function Name: checkCapture
+Purpose: To see if a card can capture multiple cards
+Parameters:
+			cardsToCheck, a vector<Card> passed by value of all the cards being captured
+Return Value: A bool for if it can be caputred. True if it can be, false if it can't be
+Local Variables:
+			int sum, the sum of all the cards being captured
+Algorithm:
+			1) Find the sum of the cards
+			2) Check to see if the value matches the card
+Assistance Received: none
+********************************************************************* */
 const bool Card::checkCapture(vector<Card> cardsToCheck) {
+	//Calculate the sum of all cards
 	int sum = 0;
 	for (unsigned int i = 0; i < cardsToCheck.size(); i++) {
 		sum += cardsToCheck[i].getNumericValue();
 	}
+	//If they exceed max of any played card, return false
 	if (sum > 14) {
 		return false; 
 	}
+	//If value matches, return true
 	if (sum == getNumericValue()) {
 		return true;
 	}
+	//If this card is an ace acting as Aces high
 	if (sum == 14 && getSymbol() == 'A') {
 		return true;
 	}
-	return false;
 
+	//Return false if fell through all above cases
+	return false;
 
 }
 
+/* *********************************************************************
+Function Name: symbolToNumericValue()
+Purpose: To convert the face symbol to its numeric value
+Parameters:
+			none (uses memeber varible symbol)
+Return Value: Void (sets member numericValue) [1-13]
+Local Variables:
+			none
+Algorithm:
+			1) Map the symbol to its numeric value
+Assistance Received: none
+********************************************************************* */
+
 void Card::symbolToNumericValue() {
+	//If its a digit, convert it to the ascii form of itself
 	if (isdigit(symbol)) {
 		numericValue = (int)(symbol - '0');
 		return;
 	}
+	//Else, map the letter to its associated value per the game rules
 	switch (symbol) {
 		case 'A':
 			numericValue = 1;
@@ -45,7 +79,7 @@ void Card::symbolToNumericValue() {
 		case 'K':
 			numericValue = 13;
 			break;
-		default:
+		default: //Catch for invalid values
 			numericValue = 1;
 			break;
 
--- c++/src/Card.h
+++ c++/src/Card.h
@@ -6,10 +6,35 @@ using namespace std;
 class Card {
 	
 public:
+	/* *********************************************************************
+Function Name: Card()
+Purpose: Intaialize Object to the invalid card
+Parameters:
+			void
+Return Value: Constructor
+Local Variables:
+			none
+Algorithm:
+			1) Intialize the card to its invalid value
+Assistance Received: none
+********************************************************************* */
 	Card() {
 		setInvalidCard();
 	}
-
+	/* *********************************************************************
+Function Name: toString
+Purpose: To format the Card in a form to be output 
+Parameters:
+			none
+Return Value: string,   The formatted string to be output
+Local Variables:
+			none
+Algorithm:
+			1) Get the suit
+			2) Get the symbol
+			3) Put both in a string as (suitSymbol)
+Assistance Received: none
+********************************************************************* */
 	virtual const string toString() const{
 		return string(1, suit)+ string(1, symbol);
 	}
@@ -17,25 +42,103 @@ public:
 	const bool checkCapture(Card);
 	const bool checkCapture(vector<Card>);
 
+	/* *********************************************************************
+Function Name: getSymbol()
+Purpose: Return the symbol on the card
+Parameters:
+			none
+Return Value: a const char representing the symbol on the card. 2-9 or A,X,J,Q,K
+Local Variables:
+			none
+Algorithm:
+			1) Get the symbol
+Assistance Received: none
+********************************************************************* */
 	const virtual char getSymbol() const{
 		return symbol;
 	}
+
+
+
+	/* *********************************************************************
+Function Name: getNumericValue()
+Purpose: To retrieve the numeric value the card represent
+Parameters:
+			none
+Return Value: a const int, which will be betwee 1-13. Aces always return 1
+Local Variables:
+			none
+Algorithm:
+			1)Return the Numeric Value
+Assistance Received: none
+********************************************************************* */
 
 	//Aces return 1
 	const virtual int getNumericValue() const{
 		return numericValue;
 	}
 
+	/* *********************************************************************
+Function Name: isInvalid()
+Purpose: Check if the card is set to the invalid card
+Parameters:
+			none
+Return Value: a const bool. True means the card is set to the invaild card, false is any other card
+Local Variables:
+			none
+Algorithm:
+			1) Check if card's suit matches the invalid suit
+Assistance Received: none
+********************************************************************* */
 	const bool isInvalid() const{
-		return suit == 'N';
+		return suit == invalidSymbol;
 	}
-
+	/* *********************************************************************
+Function Name: getSuit()
+Purpose: Get the suit on the card face
+Parameters:
+			none
+Return Value: A const char reprsenting the suit. S,Q,C,D are the valid values for non-error states 
+Local Variables:
+			temp[], an integer array used to sort the grades
+Algorithm:
+			1) Add all the grades
+			2) Divide the sum by the number of students in class to calculate the average
+Assistance Received: none
+********************************************************************* */
 	const char getSuit() const{
 		return suit;
 	}
+
+	/* *********************************************************************
+Function Name: setOwner
+Purpose: Set the owner of the card
+Parameters:
+			name, a string representing the name of the owner
+Return Value: bool, for if it completed. True if value set
+Local Variables:
+			none
+Algorithm:
+			1) set the membervariable to match the new name
+Assistance Received: none
+********************************************************************* */
 	const bool setOwner(string name){
 		ownerName = name;
+		return true;
 	}
+
+	/* *********************************************************************
+Function Name: getOwner
+Purpose: To get the owner of the card
+Parameters:
+			none
+Return Value: a const string representing the owner of the card
+Local Variables:
+			none
+Algorithm:
+			1) Retrieve the owner of the card
+Assistance Received: none
+********************************************************************* */
 	const string getOwner() const{
 		return ownerName;
 	}
@@ -48,8 +151,11 @@ protected:
 	void symbolToNumericValue();
 private:
 	int numericValue;
+
+	const char invalidSymbol = 'N';
+
 	void setInvalidCard() {
-		suit = 'N';
+		suit = invalidSymbol;
 		symbol = '0';
 	}
 

93fb4e4ec7384df7cbad3bf2ba86c310c0f9cbac
jallenNJ <33922179+jallenNJ@users.noreply.github.com>
2018-10-29 22:09:20 -0400

    Deck created and able to draw a card

---
+++ android/app/src/main/java/edu/ramapo/jallen6/oplcasino/Deck.java
@@ -0,0 +1,39 @@
+package edu.ramapo.jallen6.oplcasino;
+
+
+import java.util.Collections;
+import java.util.Vector;
+
+public class Deck {
+    private Vector<Card> deck;
+
+
+    Deck(){
+        deck = new Vector<Card>(52, 5);
+        for(CardSuit i : CardSuit.values() ){
+            if(i == CardSuit.invalid || i == CardSuit.build){
+                continue;
+            }
+            for(int j = 1; j < 14; j++){
+                deck.add(new Card(i, j));
+            }
+        }
+        Collections.shuffle(deck);
+
+
+    }
+
+
+    public boolean isEmpty(){
+        return deck.size() == 0;
+    }
+    public Card drawCard(){
+        if(isEmpty()){
+            return null;
+        }
+        Card returnCard = deck.firstElement();
+        deck.remove(0);
+        return returnCard;
+    }
+
+}
--- android/app/src/main/java/edu/ramapo/jallen6/oplcasino/Round.java
+++ android/app/src/main/java/edu/ramapo/jallen6/oplcasino/Round.java
@@ -18,5 +18,9 @@ public class Round {
 
     }
 
+    public void playRound(){
+
+    }
+
 
 }

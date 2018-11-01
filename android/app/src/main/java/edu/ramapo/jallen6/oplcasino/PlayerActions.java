8a2323d2acc6d09e97d73e200ccc520a205e85dd
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2018-10-31 20:13:07 -0400

    Added PlayerMove and its enums

---
+++ android/app/src/main/java/edu/ramapo/jallen6/oplcasino/PlayerActions.java
@@ -0,0 +1,5 @@
+package edu.ramapo.jallen6.oplcasino;
+
+public enum PlayerActions {
+    Invalid, Capture, Build, Trail;
+}
---
+++ android/app/src/main/java/edu/ramapo/jallen6/oplcasino/PlayerMove.java
@@ -0,0 +1,35 @@
+package edu.ramapo.jallen6.oplcasino;
+
+import java.util.Vector;
+
+public class PlayerMove {
+
+    private PlayerActions action;
+    private int handCardIndex;
+    private Vector<Integer> tableCardIndices;
+
+    PlayerMove(){
+        action = PlayerActions.Invalid;
+        handCardIndex = 0;
+        tableCardIndices = new Vector<Integer>(1,1);
+    }
+    PlayerMove(PlayerActions ac, int hand, Vector<Integer> tableCI){
+        action = ac;
+        handCardIndex = hand;
+        tableCardIndices = (Vector<Integer> )tableCI.clone();
+
+    }
+
+    public PlayerActions getAction(){
+        return action;
+    }
+    public int getHandCardIndex(){
+        return handCardIndex;
+    }
+    public Vector<Integer>getTableCardIndices(){
+        return tableCardIndices;
+    }
+    public int getTableCardIndiciesSize(){
+        return tableCardIndices.size();
+    }
+}

934ca096a839c93351e12a6c3c0bc7281807399e
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2018-11-01 20:40:06 -0400

    Created playerView Skeleton

--- android/app/src/main/java/edu/ramapo/jallen6/oplcasino/PlayerView.java
+++ android/app/src/main/java/edu/ramapo/jallen6/oplcasino/PlayerView.java
@@ -1,4 +1,36 @@
 package edu.ramapo.jallen6.oplcasino;
 
-public abstract class PlayerView {
+import java.util.Vector;
+
+public class PlayerView {
+    private HandView hand;
+    private int selectedIndex;
+    private HandView pile;
+    private Vector<Integer> reservedValues;
+    private Player model;
+
+
+    PlayerView(Player master){
+        model = master;
+        hand = new HandView(model.getHand(), true);
+        pile = new HandView (model.getHand(), false);
+        selectedIndex = -1;
+        reservedValues = new Vector<Integer>(2,1);
+
+    }
+
+
+    public boolean addCardToHand(Card add){
+        model.addCardToHand(add);
+        hand.createViewsFromModel();
+        return true;
+    }
+    public boolean addCardToPile(Card add){
+       // pile.addCard(add);
+        return true;
+    }
+
+
+
+
 }

0417f7b3730504c279353b1c9bb42215deb77490
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2018-11-07 23:20:34 -0500

    Human can now make capture moves (no validation)

--- android/app/src/main/java/edu/ramapo/jallen6/oplcasino/Human.java
+++ android/app/src/main/java/edu/ramapo/jallen6/oplcasino/Human.java
@@ -11,9 +11,15 @@ public class Human extends Player {
         name = "Human";
     }
     @Override
-    public PlayerMove doMove() {
+    public PlayerMove doMove(final Hand table) {
 
-        return  new PlayerMove(PlayerActions.Trail, hand.getSelectedIndices().get(0), new Vector<Integer>(1,1));
+        final Vector<Integer> selected = new Vector<Integer> (table.getSelectedIndices());
+        if(selected.size() == 0){
+            return  new PlayerMove(PlayerActions.Trail, hand.getSelectedIndices().get(0), new Vector<Integer>(1,1));
+        } else{
+            return  new PlayerMove(PlayerActions.Capture, hand.getSelectedIndices().get(0), selected);
+        }
+
 
 
     }

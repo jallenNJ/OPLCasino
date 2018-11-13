88d0b7483de3b2c528b8a4b7252e8bc0f3ed3ddc
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2018-11-13 14:16:24 -0500

    Able to see all existing files of a type

--- android/app/src/main/java/edu/ramapo/jallen6/oplcasino/LoadScreen.java
+++ android/app/src/main/java/edu/ramapo/jallen6/oplcasino/LoadScreen.java
@@ -1,8 +1,12 @@
 package edu.ramapo.jallen6.oplcasino;
 
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.File;
+import java.util.Vector;
 
 
 public class LoadScreen extends AppCompatActivity {
@@ -11,6 +15,31 @@ public class LoadScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_screen);
+
+        //Enviroment.getExternalStroagePublicDirectory(Enviroment.DIRECTORY_DOWNLOAD
+
+        Vector<String> result = new Vector<String>();
+
+        File folder =Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File[] filesInFolder = folder.listFiles(); 
+        if(filesInFolder == null){
+            return;
+        }
+        for (File file : filesInFolder) { //For each of the entries do:
+            if (file.isFile()) { //check that it's not a dir
+                //if(file.getAbsolutePath() )
+                int extensionLocated = file.getAbsolutePath().lastIndexOf(".");
+                if(extensionLocated < 0 || extensionLocated >= file.getAbsolutePath().length()){
+                    continue;
+                }
+                String extension =  file.getAbsolutePath().substring(extensionLocated);
+                if(extension.equals(".csav")){
+                    result.add(new String(file.getName()));
+                }
+            }
+        }
+
+
 
 
     }

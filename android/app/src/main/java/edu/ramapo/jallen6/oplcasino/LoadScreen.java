dd77b9a31f676ecd7fc71926bf4baeb6e9f83d1a
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2018-11-12 22:46:02 -0500

    Failed to implement file loading

--- android/app/src/main/java/edu/ramapo/jallen6/oplcasino/LoadScreen.java
+++ android/app/src/main/java/edu/ramapo/jallen6/oplcasino/LoadScreen.java
@@ -1,7 +1,9 @@
 package edu.ramapo.jallen6.oplcasino;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 public class LoadScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoadScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_screen);
-    }
+
+
+
+
+
+
 }

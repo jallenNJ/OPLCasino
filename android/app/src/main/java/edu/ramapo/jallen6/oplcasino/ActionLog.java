bb9e088cc8033c3298ff98defc7a27274249e266
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2018-11-17 22:17:17 -0500

    Formatted log per comment standards

--- android/app/src/main/java/edu/ramapo/jallen6/oplcasino/ActionLog.java
+++ android/app/src/main/java/edu/ramapo/jallen6/oplcasino/ActionLog.java
@@ -7,23 +7,30 @@ public class ActionLog {
     static private Vector<String> log;
 
 
-    static void init(){
+    public static void init(){
         log = new Vector<String>(40, 10);
     }
 
-    static String getLast(){
+    public static String getLast(){
         return log.lastElement();
     }
 
-    static void addLog(String s){
-        log.add(s);
-    }
 
-   static int getLogSize(){
+   public static int getLogSize(){
         return log.size();
    }
 
-   static String getLogEntry(int index){
+    public static void addLog(String s){
+        log.add(s);
+    }
+
+    /**
+     Get log entry at specified index
+     @param  index -- The index of log to retrieve
+     @return String at the ith location. Returns empty string if invalid index
+     */
+
+   public static String getLogEntry(int index){
         if(index < 0 || index >= log.size()){
             return "";
         }
